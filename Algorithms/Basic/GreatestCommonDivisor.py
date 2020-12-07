fbfefd237519b2fe1492ddee0a128820b88d0d3e
nitish24t <nitish24t@gmail.com>
2020-12-08 00:14:00 +0530

    refined code.

--- Algorithms/Basic/GreatestCommonDivisor.py
+++ Algorithms/Basic/GreatestCommonDivisor.py
@@ -1,17 +1,11 @@
 def findGCD(a, b):
-    print("inside GCD function")
-    print("a: " + str(a) + " b: " + str(b))
+    # medified method 1 (no need of else)
     while (a < b):
-        #print("a: " + str(a) + " b: " + str(b))
         r = b % a
-        #print("r: " + str(r))
         if r == 0:
             return a
-        else:
-            b = a
-            a = r
-
-
+        b = a
+        a = r
 
 res1 = findGCD(36, 12624)
 print(res1)
