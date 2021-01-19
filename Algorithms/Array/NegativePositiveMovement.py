f4aaf4fea0f0816ab3316d7c715554a12d562e92
Nitish Pandey <61662594+nitish24t@users.noreply.github.com>
2021-01-20 00:33:20 +0530

    Corrected The Script

--- Algorithms/Array/NegativePositiveMovement.py
+++ Algorithms/Array/NegativePositiveMovement.py
@@ -1,18 +1,19 @@
 """
 Move all negative numbers to beginning and positive to end with constant extra space - geeksforgeeks
-Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
-Output: -12 -13 -5 -7 -3 -6 11 6 5
+Input: 12, 11, -13, -5, 6, -7, 5, -3, -6
+Output: -13, -5, -7, -3, -6, 12, 5, 11, 6
+
 """
 
 def moveElements(arr):
     n = len(arr)
     j = 0
-    for i in range(n,0):
-        if (arr[i] > 0):
+    for i in range(0,n):
+        if (arr[i] < 0):
             arr[i],arr[j] = arr[j],arr[i]
             j = j + 1
     print(arr)
 
 
-arr = [-12, 11, -13, -5, 6, -7, 5, -3, -6]
+arr = [12, 11, -13, -5, 6, -7, 5, -3, -6]
 moveElements(arr)
