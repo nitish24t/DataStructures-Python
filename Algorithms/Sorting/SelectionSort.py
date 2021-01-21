9e07c55b9f1c4362843e579c6ab3736175becdcb
Nitish Pandey <61662594+nitish24t@users.noreply.github.com>
2021-01-21 23:28:09 +0530

    Stable Selection Sort (object sequence remains same)

--- Algorithms/Sorting/SelectionSort.py
+++ Algorithms/Sorting/SelectionSort.py
@@ -28,6 +28,25 @@ def selectionSort2(arr):
     print(str(arr))
 
 
+def stableSelectionSort(arr):
+    n = len(arr)
+    for i in range(n):
+
+        min_index = i
+        for j in range(i+1,n):
+            if arr[min_index] > arr[j]:
+                min_index = j
+        key = arr[min_index]
+
+        while min_index > i:
+            arr[min_index] = arr[min_index-1]
+            min_index -= 1
+
+        arr[i] = key
+    print(str(arr))
+
+
 arr = [2,5,-4,10,9]
 selectionSort(arr)
 selectionSort2(arr)
+stableSelectionSort(arr)
