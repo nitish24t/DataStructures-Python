db0dc36fc3a3f7d1c99643a860fd1b30caf3b740
nitish pandey <nitish24m@gmail.com>
2021-01-26 15:52:26 +0530

    Merge two sorted arrays - new way

--- Algorithms/Array/MergeSortedArrays.py
+++ Algorithms/Array/MergeSortedArrays.py
@@ -71,10 +71,30 @@ def mergeSortedArrays3(arr1,arr2):
         j += 1
     print(sortedMerge)
 
+
+def mergeSortedArrays3(arr1,arr2):
+    l1 = len(arr1)
+    l2 = len(arr2)
+    n = l1 + l2
+    A = [None]*n
+    i = j = 0
+
+    while i + j < n:
+        if j == l2 or (i < l1 and arr1[i] < arr2[j]):
+            A[i+j] = arr1[i]
+            i += 1
+        else:
+            A[i+j] = arr2[j]
+            j += 1
+
+    print(A)
+
+
 arr1 = [1, 2, 4, 5, 6]
 arr2 = [2, 3, 5, 7]
 mergeSortedArrays(arr1,arr2)
 mergeSortedArrays2(arr1,arr2)
+mergeSortedArrays3(arr1,arr2)
 
 
 
