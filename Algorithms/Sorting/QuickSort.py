bd6bc3859d5ff3d084b7e34bad5411c44cddd751
Nitish Pandey <61662594+nitish24t@users.noreply.github.com>
2021-02-01 00:23:39 +0530

    2nd Way In-place Quick Sort on Array

--- Algorithms/Sorting/QuickSort.py
+++ Algorithms/Sorting/QuickSort.py
@@ -36,7 +36,34 @@ def QuickSort(A,start,end):
     QuickSort(A,p+1,end)
 
 
-A = [6,2,9,5,0,8,5]
-QuickSort(A,0,len(A)-1)
-print(A)
+# A = [6,2,9,5,0,8,5]
+# QuickSort(A,0,len(A)-1)
+# print(A)
 
+
+def QuickSort2(A,start,end):
+    if start >= end:
+        return
+    pivot = A[end]
+    left,right = start,end-1
+
+    while left <= right:
+        # loop till condition fails and you find element to swap
+        while left <= right and A[left] < pivot:
+            left += 1
+        while left <= right and A[right] > pivot:
+            right -= 1
+        while left <= right:
+            A[left],A[right] = A[right],A[left]
+            left += 1
+            right -= 1
+    # put pivot in its final place
+    A[end],A[left] = A[left],A[end]
+
+    QuickSort2(A,start,left-1)
+    QuickSort2(A,left+1, end)
+
+
+A = [6,2,9,5,0,8,5]
+QuickSort2(A,0,len(A)-1)
+print(A)
